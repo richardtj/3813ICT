172e2f13bf65c4ea48a78d828fb0696a57ce8cbf
Richard Turner-Jones <richard@turner-jones.com>
2020-06-24 14:31:09 +1000

    Tasks 1-5

---
+++ Workshop 01/helloWorld.js
@@ -0,0 +1,4 @@
+// Task 5
+import HelloWorldNPM from 'hello-world-npm';
+
+console.log(HelloWorldNPM());
--- Workshop 01/index.js
+++ Workshop 01/index.js
@@ -1,14 +1,19 @@
+
+// Task 1
 console.log("Node is working");
 
+// Task 2
 let myString = "Hello@World, Node.JS Stringsss";
 let badWord = "bad word";
 let mistype = "spwlling";
-
 console.log(myString + "\n" + badWord + "\n" + mistype);
 
+// Task 3
 myString = myString.substr(0, myString.length - 2);
 myString = myString.replace("@", " ");
-
-badWord = badWord == "bad word" ?  "***" :badWord;
-
 console.log(myString + "\n" + badWord + "\n" + mistype);
+
+// Task 4
+badWord = badWord == "bad word" ?  "***" :badWord;
+console.log(myString + "\n" + badWord + "\n" + mistype);
+
